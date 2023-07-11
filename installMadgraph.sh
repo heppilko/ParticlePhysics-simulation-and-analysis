110decd8179d7c018ded2e1efe7073fb1b4e4f6f
heppilko <82280231+heppilko@users.noreply.github.com>
2023-07-11 11:47:54 +0100

    added SMEFT model to MG installation

--- installMadgraph.sh
+++ installMadgraph.sh
@@ -2,6 +2,10 @@
 curl -OL https://launchpad.net/mg5amcnlo/3.0/3.4.x/+download/MG5_aMC_v2.9.15.tar.gz
 tar -xf MG5_aMC_v2.9.15.tar.gz
 rm MG5_aMC_v2.9.15.tar.gz
+curl -OL http://feynrules.irmp.ucl.ac.be/raw-attachment/wiki/SMEFT/SMEFTsim_U35_MwScheme_UFO.tar.gz
+tar -zxvf SMEFTsim_U35_MwScheme_UFO.tar.gz
+mv SMEFTsim_U35_MwScheme_UFO MG5_aMC_v2_9_15/models/.
+rm SMEFTsim_U35_MwScheme_UFO.tar.gz
 
 echo "***********************************"
 echo "***********************************\n"
