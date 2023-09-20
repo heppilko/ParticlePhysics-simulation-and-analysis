0c35d7d6726f9ead28502c168bad743880511e9b
heppilko <82280231+heppilko@users.noreply.github.com>
2023-09-20 19:52:47 +0100

    Update analysis_python.py

--- Examples/analysis_python.py
+++ Examples/analysis_python.py
@@ -6,7 +6,7 @@ import vector
 import matplotlib.pyplot as plt
 
 # Open the .root file and extract the muon data
-tree = uproot.open('Delphes/delphes_output.root:Delphes')
+tree = uproot.open('../Delphes/delphes_output.root:Delphes')
 events = tree.arrays(['Event.Weight','Muon_size','Muon.PT','Muon.Eta','Muon.Phi'])
 
 # create filters to select the events of interest
