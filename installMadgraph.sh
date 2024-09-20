52cb64ab23b35de83e626566a3bcad43aa7f0ed0
Matt LeBlanc <matt.leblanc@cern.ch>
2024-09-19 21:47:11 -0400

    Updating to latest MadGraph and SMEFTsim versions

--- installMadgraph.sh
+++ installMadgraph.sh
@@ -1,16 +1,16 @@
 # install MG
-curl -OL https://launchpad.net/mg5amcnlo/3.0/3.4.x/+download/MG5_aMC_v2.9.15.tar.gz
-tar -xf MG5_aMC_v2.9.15.tar.gz
-rm MG5_aMC_v2.9.15.tar.gz
-curl -OL http://feynrules.irmp.ucl.ac.be/raw-attachment/wiki/SMEFT/SMEFTsim_U35_MwScheme_UFO.tar.gz
-tar -zxvf SMEFTsim_U35_MwScheme_UFO.tar.gz
-mv SMEFTsim_U35_MwScheme_UFO MG5_aMC_v2_9_15/models/.
-rm SMEFTsim_U35_MwScheme_UFO.tar.gz
+curl -OL https://launchpad.net/mg5amcnlo/3.0/3.5.x/+download/MG5_aMC_v3.5.5.tar.gz
+tar -xf MG5_aMC_v3.5.5.tar.gz
+rm MG5_aMC_v3.5.5.tar.gz
+curl -OL http://feynrules.irmp.ucl.ac.be/attachment/wiki/SMEFT/SMEFTsim_A_U35_MwScheme_UFO_v2.1.tar.gz 
+tar -zxvf SMEFTsim_A_U35_MwScheme_UFO_v2.1.tar.gz
+mv SMEFTsim_A_U35_MwScheme_UFO_v2.1 MG5_aMC_v3_5_5/models/.
+rm SMEFTsim_A_U35_MwScheme_UFO_v2.1.tar.gz
 
 echo "***********************************"
 echo "***********************************\n"
 echo "To run Madgraph interactively:"
-echo "cd MG5_aMC_v2_9_15"
+echo "cd MG5_aMC_v3_5_5"
 echo "./bin/mg5_aMC \n"
 echo "Inside the interactive environment, you will want to install lhapdf and pythia:"
 echo "install lhapdf6"
